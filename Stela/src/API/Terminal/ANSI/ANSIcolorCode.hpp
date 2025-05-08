4d68599ba4d2e0396cc0c8d158c2711780132b01
BigLands1 <151714415+BigLands1@users.noreply.github.com>
2025-05-08 18:16:48 -0400

    SRender API Added
    `SRender` is a wrapper class for OpenGL rendering functions that simplifies graphics rendering operations.
    
    Refer to the documentation for more information.

---
+++ Stela/src/API/Terminal/ANSI/ANSIcolorCode.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Stela {
+	class ANSIcolorCode {
+	#define RESET   "\033[0m"
+	#define RED     "\033[31m"
+	#define GREEN   "\033[32m"
+	#define YELLOW  "\033[33m"
+	#define BLUE    "\033[34m"
+	#define MAGENTA "\033[35m"
+	#define CYAN    "\003[36m"
+	};
+}
