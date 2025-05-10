a2effb57714b77b2be447e910c19c9b5e54b3cd3
BigLands1 <151714415+BigLands1@users.noreply.github.com>
2025-05-10 00:22:39 -0400

    Updated Editor UI
    Updated Editor UI

--- Stela/src/Main.cpp
+++ Stela/src/Main.cpp
@@ -2,6 +2,7 @@
 #include <API/Input/SInput.hpp>
 #include <API/Terminal/ANSI/ANSIcolorCode.hpp>
 #include <API/Render/SRender.hpp>
+#include <API/UI/Theme.hpp>
 
 #include <imgui/imgui.h>
 #include <imgui/backends/imgui_impl_glfw.h>
@@ -36,6 +37,9 @@ int main() {
    // Setup Platform/Renderer backends
    ImGui_ImplGlfw_InitForOpenGL(SWindow.GetGLFWwindow(), true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
    ImGui_ImplOpenGL3_Init();
+
+   // Setup custom theme
+   Stela::UI::SetupTheme();
 
    SRender.initializeBuffers();
    unsigned int VBO = SRender.VBO;
@@ -76,6 +80,7 @@ int main() {
    SRender.EnableVertexAttribArray(0);
 
    bool wireframe = false;
+   bool show_properties = false;
 
    while (!SWindow.ShouldClose()) {
 	   SWindow.PollEvents();
@@ -85,8 +90,23 @@ int main() {
 	   ImGui_ImplGlfw_NewFrame();
 	   ImGui::NewFrame();
 
+       // Create the main menu bar
+       if (ImGui::BeginMainMenuBar()) {
+           if (ImGui::BeginMenu("File")) {
+               if (ImGui::MenuItem("Exit", "Alt+F4")) {
+                   glfwSetWindowShouldClose(SWindow.GetGLFWwindow(), true);
+               }
+               ImGui::EndMenu();
+           }
+           if (ImGui::BeginMenu("Tools")) {
+               ImGui::MenuItem("Properties", NULL, &show_properties);
+               ImGui::EndMenu();
+           }
+           ImGui::EndMainMenuBar();
+       }
+
 	   // Create a fullscreen window for the dockspace
-	   ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+	   ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDocking;
 	   ImGuiViewport* viewport = ImGui::GetMainViewport();
 	   ImGui::SetNextWindowPos(viewport->WorkPos);
 	   ImGui::SetNextWindowSize(viewport->WorkSize);
@@ -106,13 +126,15 @@ int main() {
 
 	   ImGui::End();
 	   
+       // Properties window
+       if (show_properties) {
+           ImGui::Begin("Properties", &show_properties);
+           ImGui::Text("Wireframe Mode");
+           ImGui::Checkbox("Wireframe", &wireframe);
+           ImGui::End();
+       }
 
-	   ImGui::Begin("Properties");
-	   ImGui::Text("Wireframe Mode");
-	   ImGui::Checkbox("Wireframe", &wireframe);
-	   ImGui::End();
-
-
+	   // Render OpenGL content
 	   SWindow.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	   SWindow.ClearColorBuffer();
 	   SRender.UseProgram(shaderProgram);
@@ -126,6 +148,7 @@ int main() {
 		   SRender.PolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	   }
 
+	   // Render ImGui
 	   ImGui::Render();
 	   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
