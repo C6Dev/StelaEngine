c829c49347470add56fcb0969dbc4f12de754d38
BigLands1 <151714415+BigLands1@users.noreply.github.com>
2025-05-18 17:34:10 -0400

    revert

--- Stela/src/Main.cpp
+++ Stela/src/Main.cpp
@@ -2,12 +2,6 @@
 #include <API/Input/SInput.hpp>
 #include <API/Terminal/ANSI/ANSIcolorCode.hpp>
 #include <API/Render/SRender.hpp>
-#include <API/UI/Theme.hpp>
-#include "API/Config.hpp"
-
-#include <imgui/imgui.h>
-#include <imgui/backends/imgui_impl_glfw.h>
-#include <imgui/backends/imgui_impl_opengl3.h>
 
 float vertices[] = {
 	 0.0f,  0.5f, 0.0f,  // Top center
@@ -24,35 +18,8 @@ int main() {
    Stela::SWindow SWindow(800, 600, "Stela");
    Stela::SInput SInput;
    SInput.Init(SWindow.GetGLFWwindow());
-
-   // Load window position and size from config
-   if (Stela::Config::ConfigManager::GetInstance().LoadConfig()) {
-       glfwSetWindowPos(SWindow.GetGLFWwindow(), 
-           Stela::Config::ConfigManager::GetInstance().GetValue<int>("WindowPosX", 100),
-           Stela::Config::ConfigManager::GetInstance().GetValue<int>("WindowPosY", 100));
-       glfwSetWindowSize(SWindow.GetGLFWwindow(),
-           Stela::Config::ConfigManager::GetInstance().GetValue<int>("WindowWidth", 800),
-           Stela::Config::ConfigManager::GetInstance().GetValue<int>("WindowHeight", 600));
-   }
-
    const GLchar* vertexShaderSource = SRender.loadShaderSource("Shaders/VertexShader.vert");
    const GLchar* fragmentShaderSource = SRender.loadShaderSource("Shaders/FragmentShader.frag");
-
-   // Setup Dear ImGui context
-   IMGUI_CHECKVERSION();
-   ImGui::CreateContext();
-   ImGuiIO& io = ImGui::GetIO();
-   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-   io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-   io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-   io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
-
-   // Setup Platform/Renderer backends
-   ImGui_ImplGlfw_InitForOpenGL(SWindow.GetGLFWwindow(), true);
-   ImGui_ImplOpenGL3_Init();
-
-   // Setup custom theme
-   Stela::UI::SetupTheme();
 
    SRender.initializeBuffers();
    unsigned int VBO = SRender.VBO;
@@ -92,85 +59,10 @@ int main() {
    SRender.setVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
    SRender.EnableVertexAttribArray(0);
 
-   bool wireframe = Stela::Config::ConfigManager::GetInstance().GetValue<bool>("Wireframe", false);
-   bool show_properties = Stela::Config::ConfigManager::GetInstance().GetValue<bool>("ShowProperties", false);
+   bool wireframe = false;
 
    while (!SWindow.ShouldClose()) {
 	   SWindow.PollEvents();
-
-       // Save window position and size when they change
-       int xpos, ypos, width, height;
-       glfwGetWindowPos(SWindow.GetGLFWwindow(), &xpos, &ypos);
-       glfwGetWindowSize(SWindow.GetGLFWwindow(), &width, &height);
-       
-       auto& config = Stela::Config::ConfigManager::GetInstance();
-       if (xpos != config.GetValue<int>("WindowPosX", 100) || 
-           ypos != config.GetValue<int>("WindowPosY", 100) ||
-           width != config.GetValue<int>("WindowWidth", 800) || 
-           height != config.GetValue<int>("WindowHeight", 600)) {
-           config.SetValue("WindowPosX", xpos);
-           config.SetValue("WindowPosY", ypos);
-           config.SetValue("WindowWidth", width);
-           config.SetValue("WindowHeight", height);
-       }
-
-	   // Start the Dear ImGui frame
-	   ImGui_ImplOpenGL3_NewFrame();
-	   ImGui_ImplGlfw_NewFrame();
-	   ImGui::NewFrame();
-
-       // Create the main menu bar
-       if (ImGui::BeginMainMenuBar()) {
-           if (ImGui::BeginMenu("File")) {
-               if (ImGui::MenuItem("Exit", "Alt+F4")) {
-                   glfwSetWindowShouldClose(SWindow.GetGLFWwindow(), true);
-               }
-               ImGui::EndMenu();
-           }
-           if (ImGui::BeginMenu("Tools")) {
-               ImGui::MenuItem("Properties", NULL, &show_properties);
-               if (ImGui::IsItemEdited()) {
-                   config.SetValue("ShowProperties", show_properties);
-               }
-               ImGui::EndMenu();
-           }
-           ImGui::EndMainMenuBar();
-       }
-
-	   // Create a fullscreen window for the dockspace
-	   ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDocking;
-	   ImGuiViewport* viewport = ImGui::GetMainViewport();
-	   ImGui::SetNextWindowPos(viewport->WorkPos);
-	   ImGui::SetNextWindowSize(viewport->WorkSize);
-	   ImGui::SetNextWindowViewport(viewport->ID);
-	   ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-	   ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-	   window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-	   window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-	   window_flags |= ImGuiWindowFlags_NoBackground;
-
-	   ImGui::Begin("Editor", nullptr, window_flags);
-	   ImGui::PopStyleVar(2);
-
-	   // DockSpace ID
-	   ImGuiID dockspace_id = ImGui::GetID("EditorDockspace");
-	   ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
-
-	   ImGui::End();
-	   
-       // Properties window
-       if (show_properties) {
-           ImGuiWindowFlags properties_flags = ImGuiWindowFlags_NoCollapse;
-           ImGui::Begin("Properties", &show_properties, properties_flags);
-           ImGui::Text("Wireframe Mode");
-           ImGui::Checkbox("Wireframe", &wireframe);
-           if (ImGui::IsItemEdited()) {
-               config.SetValue("Wireframe", wireframe);
-           }
-           ImGui::End();
-       }
-
-	   // Render OpenGL content
 	   SWindow.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	   SWindow.ClearColorBuffer();
 	   SRender.UseProgram(shaderProgram);
@@ -183,19 +75,6 @@ int main() {
 	   else {
 		   SRender.PolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	   }
-
-	   // Render ImGui
-	   ImGui::Render();
-	   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-       // Update and Render additional Platform Windows
-       if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-           GLFWwindow* backup_current_context = glfwGetCurrentContext();
-           ImGui::UpdatePlatformWindows();
-           ImGui::RenderPlatformWindowsDefault();
-           glfwMakeContextCurrent(backup_current_context);
-       }
-
        SWindow.SwapBuffers();
        SInput.Input(GLFW_KEY_ESCAPE, [&] {
            glfwSetWindowShouldClose(SWindow.GetGLFWwindow(), true);
@@ -206,14 +85,10 @@ int main() {
 		   }
 		   else if (wireframe == false) {
 			   wireframe = true;
-           }
-           config.SetValue("Wireframe", wireframe);
+		   }
 		   });
    }
 
-   ImGui_ImplOpenGL3_Shutdown();
-   ImGui_ImplGlfw_Shutdown();
-   ImGui::DestroyContext();
    SRender.~SRender();
    SWindow.~SWindow();
    std::cout << RESET;
